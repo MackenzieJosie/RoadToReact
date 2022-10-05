6f489af53317ad9a2df98a54e2d8fb039dccb6ed
Mackenzie <mackenzie.josie@gmail.com>
2022-10-05 14:22:42 -0400

    react component declaration

--- src/App.jsx
+++ src/App.jsx
@@ -23,38 +23,49 @@ const list = [
   },
 ];
 
-function getTitle(title) {
+const getTitle = (title) => {
   return title;
 }
 
-function App() {
+const App = () => {
   return (
     <div>
       <h1>My Hacker Stories</h1>
       {/* <h1>{welcome.greeting} {welcome.title}</h1> */}
 
-      <label htmlFor="search">Search: </label>
-      <input id="search" type="text" />
+      <Search />
 
       <hr />
 
-      <ul>
-        {list.map(function (item) {
-          return (
-            <li key={item.objectID}>
-              <span>
-                <a href={item.url}>{item.title}</a>
-                <span>{item.author}</span>
-                <span>{item.num_comments}</span>
-                <span>{item.points}</span>
-              </span>
-              
-            </li>
-          );
-        })}
-      </ul>
+      <List />
+      <List />
     </div>
   );
 }
+
+const Search = () => (
+  <div>
+    <label htmlFor="search">Search: </label>
+    <input id="search" type="text" />
+  </div>
+);
+
+const List = () => (
+  <ul>
+    {list.map( (item) => {
+      return (
+        <li key={item.objectID}>
+          <span>
+            <a href={item.url}>{item.title}</a>
+            <span>{item.author}</span>
+            <span>{item.num_comments}</span>
+            <span>{item.points}</span>
+          </span>
+          
+        </li>
+      );
+    })}
+  </ul>
+);
 
 export default App;
